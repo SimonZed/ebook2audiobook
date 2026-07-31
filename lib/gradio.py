af666451a5d82a31f9d433d8fd52965a89beeb30
unknown <admin@radsl.net>
2026-07-31 16:39:36 -0700

    ...

--- lib/gradio.py
+++ lib/gradio.py
@@ -883,7 +883,7 @@ def build_interface(args:dict)->gr.Blocks:
                             gr_abs_api_token = gr.Textbox(label='API Token', elem_id='gr_abs_api_token', value=default_abs_api_token, type='password', placeholder='eyJ...', lines=1, max_lines=1, interactive=True, scale=1) 
                         with gr.Row(elem_id='gr_row2_abs'):
                             gr_abs_library_id = gr.Dropdown(label='', elem_id='gr_abs_library_id', choices=[('Enter URL + API Token to load libraries', '')], value=default_abs_library_id or None, interactive=True, scale=2)
-                            gr_abs_search_btn = gr.Button(elem_id='gr_abs_search_btn', value='🔍', elem_classes=['gr-abs-search-btn'], variant='secondary', interactive=True, scale=1)
+                            gr_abs_search_btn = gr.Button('🔍', elem_id='gr_abs_search_btn', elem_classes=['small-btn-lock'], variant='secondary', visible=True, interactive=True, scale=0, min_width=60)
                         with gr.Group(elem_id='gr_group_abs_upload_btn', elem_classes=['gr-group-abs-upload-btn']):
                             gr_abs_status = gr.Textbox(elem_id='gr_abs_status', label='Status', lines=1, max_lines=1, interactive=False, visible=True)
                             gr_abs_upload_btn = gr.Button(elem_id='gr_abs_upload_btn', value='🡅', elem_classes=['gr-abs-upload-btn'], variant='secondary', interactive=False)
