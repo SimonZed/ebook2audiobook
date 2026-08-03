04dcb5027358864b1ec0eb764bb857572fe22f0f
unknown <admin@radsl.net>
2026-08-03 06:39:42 -0700

    ...

--- app.py
+++ app.py
@@ -231,9 +231,9 @@ SML tags available:
     headless_optional_group.add_argument(cli_options[29], action='version', version=f'ebook2audiobook version {prog_version}', help='''Show the version of the script and exit''')
     headless_optional_group.add_argument(cli_options[30], type=str, default='', help='''Audiobookshelf server URL (e.g. http://localhost:13378).''')
     headless_optional_group.add_argument(cli_options[31], type=str, default='', help='''Audiobookshelf API token.''')
-    headless_optional_group.add_argument(cli_options[32], type=str, default='', help='''Audiobookshelf library (full path required).''')
+    headless_optional_group.add_argument(cli_options[32], type=str, default='', help='''Audiobookshelf library ID.''')
+    headless_optional_group.add_argument(cli_options[33], action='store_true', help=argparse.SUPPRESS)
     headless_optional_group.add_argument(cli_options[34], action='store_true', help=argparse.SUPPRESS)
-    headless_optional_group.add_argument(cli_options[35], action='store_true', help=argparse.SUPPRESS)
 
     for arg in sys.argv:
         if arg.startswith('--') and arg not in cli_options:
