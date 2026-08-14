63cd0b7365fb7492a8a0a1364852893abe8cfe97
unknown <admin@radsl.net>
2026-08-14 06:33:43 -0700

    ...

--- lib/gradio.py
+++ lib/gradio.py
@@ -1066,15 +1066,14 @@ def build_interface(args:dict)->gr.Blocks:
                 outputs = tuple([gr.update(interactive=False) for _ in range(10)])
                 return outputs + (gr.update(visible='hidden'), gr.update(visible='hidden'))
 
-            def _enable_on_voice_upload(session_id:str, ebook_src:str, ebook_textarea:str)->tuple:
+            def _enable_on_voice_upload(session_id:str, ebook_src:str, ebook_textarea:str, ebook_mode:str)->tuple:
                 visible_buttons = 'hidden'
                 enabled_convert_btn = False
                 session = context.get_session(session_id)
                 if session and session.get('id', False):
                     outputs = tuple([gr.update(interactive=True) for _ in range(9)])
-                    if session and session.get('id', False):
-                        enabled_convert_btn = True if ebook_src is not None and ebook_textarea is not None else enabled_convert_btn
-                        visible_buttons = True if session['voice'] is not None else visible_buttons
+                    enabled_convert_btn = True if (ebook_src and ebook_mode != ebook_modes['TEXT']) or (ebook_textarea and ebook_mode == ebook_modes['TEXT']) else enabled_convert_btn
+                    visible_buttons = True if session['voice'] is not None else visible_buttons
                 return outputs + (gr.update(interactive=enabled_convert_btn), gr.update(visible=visible_buttons), gr.update(visible=visible_buttons))
 
             def _disable_on_custom_upload()->tuple:
@@ -3053,7 +3052,7 @@ def build_interface(args:dict)->gr.Blocks:
                 show_progress_on=[gr_voice_list]
             ).then(
                 fn=_enable_on_voice_upload,
-                inputs=[gr_session, gr_ebook_src, gr_ebook_textarea],
+                inputs=[gr_session, gr_ebook_src, gr_ebook_textarea, gr_ebook_mode],
                 outputs=outputs_on_voice_upload,
                 show_progress_on=[gr_voice_list]
             )
