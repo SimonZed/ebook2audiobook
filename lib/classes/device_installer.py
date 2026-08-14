81960974d93814eb3a0e0ebbf1c310a5c72c8c8c
unknown <admin@radsl.net>
2026-08-14 16:21:30 -0700

    ...

--- lib/classes/device_installer.py
+++ lib/classes/device_installer.py
@@ -1652,7 +1652,7 @@ class DeviceInstaller():
                     msg = f'---> Hardware detected: {device_info}'
                     print(msg)
                     #tag = device_info.get('tag')
-                    tag = 'cu126'
+                    tag = 'cu129'
                     if tag in ['unknown','unsupported']:
                         return 0
                     key = 'last' if self.python_version >= (3, 12) else 'base'
