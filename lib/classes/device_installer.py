9cc5864178f349c8db840364e0f1358171ae4d7e
unknown <admin@radsl.net>
2026-08-14 18:06:21 -0700

    ...

--- lib/classes/device_installer.py
+++ lib/classes/device_installer.py
@@ -1668,6 +1668,7 @@ class DeviceInstaller():
                     msg = f'---> Hardware detected: {device_info}'
                     print(msg)
                     #tag = device_info.get('tag')
+                    print(f"--------------{device_info.get('tag')}---------------")
                     tag = 'win-cu128'
                     if tag in ['unknown','unsupported']:
                         return 0
